4f1ae6a8d14b818f0df9a68df3c25652ab455712
ponuch <ponuch.lug@gmail.com>
2022-12-18 16:35:52 +0300

    поправил задание №2

--- src/main/java/lesson3/second_task/SecondTask.java
+++ src/main/java/lesson3/second_task/SecondTask.java
@@ -1,5 +1,6 @@
 package lesson3.second_task;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
@@ -10,9 +11,21 @@ import java.util.stream.IntStream;
  */
 public class SecondTask {
     public static void main(String[] args) {
+        //фильтрация
         var random = new Random();
         var intStream = IntStream.generate(() -> random.nextInt(100) );
         var result = intStream.limit(20).filter(n -> n % 2 != 0).toArray();
         System.out.println(Arrays.toString(result));
+
+        //именно удаление
+
+        var data = new ArrayList<Integer>();
+        for (int i = 0; i < 20; i++) {
+            data.add((int)(Math.random() * 100));
+        }
+
+        System.out.println("Before removing %s".formatted(data));
+        data.removeIf(n -> n % 2 == 0);
+        System.out.println("After removing %s".formatted(data));
     }
 }
